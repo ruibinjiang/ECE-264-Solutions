6c222fd19b71827a8d4e7f04a071c1c7d89871f1
ruibinjiang <jiang551@purdue.edu>
2018-08-27 17:59:04 -0400

    HW02 Finished

--- HW02/sort.c
+++ HW02/sort.c
@@ -33,9 +33,28 @@ void ssort(int * arr, int size)
 
   // After finding the smallest element among the unsorted elements,
   // swap it with the element of the index from the first level
-  
+
   // call checkOrder to see whether this function correctly sorts the
   // elements
+  int sorted_index;
+  int unsorted_index;
+  int temp;
+  int min_index;
+
+  for(sorted_index = 0; sorted_index < size-1; sorted_index++){
+    //find index of the minimum number in the array
+    min_index = sorted_index;
+    for(unsorted_index = sorted_index; unsorted_index < size; unsorted_index++){
+        if(arr[unsorted_index] < arr[min_index])
+          min_index = unsorted_index;
+    }
+
+    //switch the minimum number found with the first unsorted number
+    temp = arr[min_index];
+    arr[min_index] = arr[sorted_index];
+    arr[sorted_index] = temp;
+  }
+
   if (checkOrder(arr, size) == false)
     {
       fprintf(stderr, "checkOrder returns false\n");
