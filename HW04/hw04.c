c15b1b98fe996c7c60ee48da9461a537d5b59d29
ruibinjiang <jiang551@purdue.edu>
2018-09-10 10:34:16 -0400

    HW04 Edit

--- HW04/hw04.c
+++ HW04/hw04.c
@@ -4,11 +4,11 @@
 // Modify only between the block which mentions what to modiy
 // ===== DO NOT MODIFY BELOW THIS ======
 #include "hw04.h"
-  
+
 void cleanup (FILE * fpin)
 {
   fclose (fpin);
-}  
+}
 
 // read the data, return true if success, false if fail
 bool readData (FILE * fpin, DataPoint * *dp, int nval, int dim)
@@ -27,7 +27,7 @@ bool readData (FILE * fpin, DataPoint * *dp, int nval, int dim)
   return true;
 }
 
- 
+
 // write the output centroids to the file
 // check for all the NULL before calling this function, it does not check
 void writeCentroids (const char *filename, Centroid * *centroids, int kval)
@@ -48,11 +48,11 @@ void writeCentroids (const char *filename, Centroid * *centroids, int kval)
   fclose (fpout);
 }
 
-  
+
 //===== DO NOT MODIFY ABOVE THIS ======
 
 // Complete the below functions only, do not modify any other function.
-  
+
 #ifdef TEST_DIST
 // distance - funtion to get the distance between present centroid and datapoint
 // @param DataPoint * - pointer to DataPoint structure from which distance needs to be calculated
@@ -62,14 +62,19 @@ distance (const DataPoint * datapoint, const Centroid * centroid)
 {
   // since this is for comparison only, there is no need to call sqrt
   long long int sum = 0;	// must initialize to zero
+  long long int diff;
   // find Euclidean distance and then return 'sum' without calling sqrt
+  for (int i = 0; i < centroid -> dimension; i++){
+    diff = centroid -> data[i] - datapoint -> data[i];
+    sum = sum + diff * diff;
+  }
 	return sum;
 }
 
-#endif	
-   
+#endif
+
 #ifdef TEST_CLOSESTCENTROID
-  
+
 // for a data point, find the closest centroid
 // 1. calculate the distance between the data point and the first
 // centroid, set it to the minimum distance
@@ -78,15 +83,22 @@ distance (const DataPoint * datapoint, const Centroid * centroid)
 // centroid from which the distance is smaller than previously seen,
 int closestCentroid (int kval, DataPoint * datapoint, Centroid * *centroids)
 {
-  int mindex; //index of the closest centroid 
+  int mindex = 0; //index of the closest centroid
+  long long int diff = distance(datapoint, centroids[mindex]);
   // Please note that return value of distance is long long int, so initialize the values with the same type
   // go through each centroid and find the distance
   // keep track of minimum difference and index of centroid which has the smallest distance
+  for (int i = 0; i < kval; i++){
+    if (distance(datapoint, centroids[i]) < diff){
+      diff = distance(datapoint, centroids[i]);
+      mindex = i;
+    }
+  }
   return mindex;
 }
 
-#endif	
-    
+#endif
+
 #ifdef TEST_KMEAN
 // kmean - function which finds the k clusters in the data set
 // kval - # of clusters
@@ -98,27 +110,27 @@ int closestCentroid (int kval, DataPoint * datapoint, Centroid * *centroids)
 void kmean (int kval, int nval, DataPoint * *datapoints, Centroid * *centroids)
 {
 		// reset all centroids
-		
+
 		// initialize each data point to a cluster between 0 and kval - 1
 
 		// find the centroid for initial random assignment of datapoints
-		
+
 		// Now start the loop till convergence is met - (Please see README for understanding kmean algorithm convergence condition)
-		// 
+		//
 		// 1. for each data point, find the index of the centroid that is the closest
 		// 2. store that index in DataPoint's structure's cluster value.
 		// 3. reset all the centroids
 		// 4. go through each datapoint again and add this datapoint to its centroid using Centroid_addPoint function
-		// 5. find the new centroid for each cluster by calling Centroid_findCenter 
+		// 5. find the new centroid for each cluster by calling Centroid_findCenter
 }
- 
-#endif	
-  
-/*===== DO NOT MODIFY BELOW THIS ======*/ 
+
+#endif
+
+/*===== DO NOT MODIFY BELOW THIS ======*/
 int
 main (int argc, char * *argv)
 {
-  
+
   // argv[1]: name of input file
   // argv[2]: value of k/ number of centroids
   // argv[3]: name of output file
@@ -127,7 +139,7 @@ main (int argc, char * *argv)
       fprintf (stderr, "argc is %d, not 4\n", argc);
       return EXIT_FAILURE;
     }
-   
+
   // opening file to read the data points from
   FILE * fpin = fopen (argv[1], "r");
   if (fpin == NULL)
@@ -135,7 +147,7 @@ main (int argc, char * *argv)
       fprintf (stderr, "fopen %s fail\n", argv[1]);
       return EXIT_FAILURE;
     }
-   
+
   // convert long to int
   int kval = (int) strtol (argv[2], NULL, 10);
   if (kval <= 0)
@@ -146,7 +158,7 @@ main (int argc, char * *argv)
   // control the random number sequence
   int randseed = 1729;	// any integer will do, DO NOT CHANGE
   srand (randseed);
-    
+
   // getting number of datapoints
   int nval;
   fscanf (fpin, "%d", &nval);
@@ -157,7 +169,7 @@ main (int argc, char * *argv)
       cleanup (fpin);
       return EXIT_FAILURE;
     }
-   
+
   // getting dimensions of the data from the file
   int dim;
   fscanf (fpin, "%d", &dim);
@@ -168,7 +180,7 @@ main (int argc, char * *argv)
       cleanup (fpin);
       return EXIT_FAILURE;
     }
-    
+
   // allocate memory for the data points
   DataPoint * *datapoint_array = DataPoint_createArray (nval, dim);
   if (datapoint_array == NULL)
@@ -177,9 +189,9 @@ main (int argc, char * *argv)
       cleanup (fpin);
       return EXIT_FAILURE;
     }
-  
+
   // allocate memory for array of centroids
-  Centroid * *centroids = Centroid_createArray (kval, dim);	
+  Centroid * *centroids = Centroid_createArray (kval, dim);
   if (centroids == NULL)
     {
       printf ("Error in creating centroids array\n");
@@ -187,7 +199,7 @@ main (int argc, char * *argv)
       cleanup (fpin);
       return EXIT_FAILURE;
     }
-   
+
   // read the data from the file
   if (readData (fpin, datapoint_array, nval, dim) == false)
     {
@@ -196,7 +208,7 @@ main (int argc, char * *argv)
       DataPoint_freeArray (datapoint_array, nval);
       return EXIT_FAILURE;
     }
-     
+
   // calling kmean function to find the cetroids
   kmean (kval, nval, datapoint_array, centroids);
 
@@ -208,5 +220,3 @@ main (int argc, char * *argv)
   cleanup (fpin);
   return EXIT_SUCCESS;
 }
-
-
