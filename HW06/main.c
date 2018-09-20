c2498cb16c05e5c081408f229da142f7be20b96f
ruibinjiang <jiang551@purdue.edu>
2018-09-20 17:49:37 -0400

    Update HW06

--- HW06/main.c
+++ HW06/main.c
@@ -2,9 +2,9 @@
 // *** Please modify this file, as indicated
 // ***
 
-#include <stdio.h>  
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "vector.h"
 
@@ -14,25 +14,32 @@
 int main(int argc, char * * argv)
 {
     // input file will be specified from the terminal - test files are made available in inputs folder
-
     // check for missing input file/output file, if so, "return EXIT_FAILURE;"
-
+    if (argc != 3){
+      return EXIT_FAILURE;
+    }
     // store the input file name in a variable, from argv[1]. For example, char* in_file = argv[1];
     // store the output file name in a variable, from argv[2]. For example, char* out_file = argv[2];
-
+    char* bin_in = argv[1];
+    char* bin_out = argv[2];
     // call numberOfElements(in_file) to obtain the number of elements in the file.
-
+    int count = numberOfElements(bin_in);
     // allocate memory to store the numbers (initialize an array of structures)
             // check for malloc fail, if so, "return EXIT_FAILURE;"
-  
+     Vector * in_list = malloc(count * sizeof(Vector));
+     if (in_list == NULL){
+       return EXIT_FAILURE;
+     }
     //call the fillVector(vector, count, in_file); and store the values from the file in the array of strucures.
-  
+    fillVector(in_list, count, bin_in);
 
     //Use qsort() function, after defining the comparator() function.
             //ENSURE THE COMPARATOR FUNCTION IS USED TO SORT THE ARRAY OF STRUCTURES W.R.T. THE STRUCTURE VARIABLE "x".
-  
+    qsort(in_list, count, sizeof(Vector), cmp);
 
     //call the writeFile(vector,count, out_file); to save the sorted vector into the file.
+    writeFile(in_list, count, bin_out);
+    free(in_list);
     return EXIT_SUCCESS;
 }
 #endif
--- HW06/vector.c
+++ HW06/vector.c
@@ -1,43 +1,65 @@
 //
 // MODIFY THIS FILE AS DIRECTED
 //
-#include <stdio.h>  
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "vector.h"
 
 
 
 #ifdef TEST_COMP
-int cmp(const void *a, const void *b) 
-{ 
+int cmp(const void *a, const void *b)
+{
     //typecast the inputs into Vector * form.
         //For example: Vector *typecasted_b = (Vector *)b;
+    Vector * vec_a = (Vector *)a;
+    Vector * vec_b = (Vector *)b;
     //compare typecased_a->x and typecasted_b->x, and return appropriate values.
-} 
+    if (vec_b->x > vec_a->x){
+      return -1;
+    }
+    else if (vec_b->x == vec_a->x){
+      return 0;
+    }
+    return 1;
+}
 #endif
 
 #ifdef TEST_COUNT
 int numberOfElements(char* in_file_name)
 {
-    //initialize file_pointer, and open file in read mode. 
-        //Please note that we are working with binary files. 
-    
+    //initialize file_pointer, and open file in read mode.
+        //Please note that we are working with binary files.
+    FILE *  file_pointer= fopen(in_file_name, "r");
+    if (file_pointer == NULL){
+      return EXIT_FAILURE;
+    }
     //use fseek(file_pointer, 0, SEEK_END); to point to the end of the file.
+    fseek(file_pointer, 0, SEEK_END);
     //use long where = ftell(file_pointer); to get the size of the file.
+    long end_file = ftell(file_pointer);
     //use float numelem = where / sizeof(Vector); to obtain the number of elements
+    float num_elem = end_file / sizeof(Vector);
     //return numelem; to send the number of elements back to main().
+    fclose(file_pointer);
+    return num_elem;
 }
 #endif
 
 #ifdef TEST_FILL
 void fillVector(Vector* vector, int count, char * in_file_name)
 {
-    // open binary file to read after initializing file_pointer 
-   
+    // open binary file to read after initializing file_pointer
+    FILE *  file_pointer= fopen(in_file_name, "r");
+    if (file_pointer == NULL){
+      return;
+    }
     //How to read the file?
         //use fread(vector, sizeof(Vector), count, file_pointer)
+    fread(vector, sizeof(Vector), count, file_pointer);
+    fclose(file_pointer);
 }
 #endif
 
@@ -45,8 +67,13 @@ void fillVector(Vector* vector, int count, char * in_file_name)
 void writeFile(Vector* vector, int count, char* out_file_name)
 {
     // open binary file to write after initializing file_pointer
-   
+    FILE *  file_pointer= fopen(out_file_name, "w");
+    if (file_pointer == NULL){
+      return;
+    }
     //How to write to the file?
         //use fwrite(vector, sizeof(Vector), count, file_pointer)
+    fwrite(vector, sizeof(Vector), count, file_pointer);
+    fclose(file_pointer);
 }
 #endif
