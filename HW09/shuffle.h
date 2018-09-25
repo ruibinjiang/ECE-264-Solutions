fccd0f790f26de654c13e55fb9a8cba963d6a845
ruibinjiang <jiang551@purdue.edu>
2018-09-25 19:13:24 -0400

    Update files

--- HW09/shuffle.h
+++ HW09/shuffle.h
@@ -1,13 +1,13 @@
 // do not modify this file
-#ifdef CARD_SHUFFLING_H
+#ifndef CARD_SHUFFLING_H
 #define CARD_SHUFFLING_H
 #include "utility.h"
 
 void divide(CardDeck orig_deck, CardDeck* upper_deck, CardDeck* lower_deck);
 
-void interleave(CardDeck upper_deck, CardDeck lower_deck);
+void interleave(CardDeck upper_deck, CardDeck lower_deck, int repeat);
 
-void shuffle(CardDeck orig_deck);
+void shuffle(CardDeck orig_deck, int repeat);
 
 void repeat_shuffle(CardDeck orig_deck, int k);
 
