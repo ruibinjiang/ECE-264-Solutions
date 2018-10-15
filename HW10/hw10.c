1cf59e097c135ee72269d5d36dbeff94aa6f502f
ruibinjiang <42720074+ruibinjiang@users.noreply.github.com>
2018-10-15 16:44:21 -0400

    Update HW10

--- HW10/hw10.c
+++ HW10/hw10.c
@@ -4,15 +4,17 @@
 
 int main(int argc, char **argv){
   // check the arguments
-	if(argc != 2){return EXIT_FAILURE;}
+	if(argc != 3){return EXIT_FAILURE;}
   // open the BMP file
 	BMPImage * image = BMP_Open(argv[1]);
   // convert to gray scale
 	BMPImage * image = ImgToGray(image);
 	// check for error in converting to gray scale
-
+	if(image == NULL){return EXIT_FAILURE;}
 	// write the gray image to file
+	BMP_Write(argv[2], image);
 	// free all the images
+	BMP_Free(image);
 	return EXIT_SUCCESS;
 }
 
