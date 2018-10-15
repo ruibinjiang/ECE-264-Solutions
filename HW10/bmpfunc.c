b7b8e4dca1831718236a74b1f83bc032dc6eb143
ruibinjiang <42720074+ruibinjiang@users.noreply.github.com>
2018-10-15 16:37:23 -0400

    Update HW10

--- HW10/bmpfunc.c
+++ HW10/bmpfunc.c
@@ -23,17 +23,20 @@ BMPImage * ImgToGray(BMPImage * image){
 	// the image has the same size
 	// therefore the header has to stay the same
 	// check for memory allocation failure 
-	
+	BMPImage *gray_image = (BMPImage *)malloc(sizeof(BMPImage));
+	if (gray_image == NULL){return NULL;}
 	//gray_image can be the name of the new image memory allotment
-	//gray_image->header = image->header;
+	gray_image->header = image->header;
 	
 	//Assign the the imagesize as height * width
-	// (gray_image->header).imagesize = (gray_image->header).width*(gray_image->header).height;
+	(gray_image->header).imagesize = (gray_image->header).width*(gray_image->header).height;
 	
 	//check for data allocation failure using :
-	//(gray_image->data = malloc(sizeof(unsigned char)*(gray_image->header).imagesize)) == NULL)
+	gray_image->data = malloc(sizeof(unsigned char)*(gray_image->header).imagesize));
+	if(gray_image->data == NULL){return NULL;}
 	
 	int pixel=0;
+	int gray_val = 0;
 	//Run loop for all pixels using height and width
 	//convert each pixel of all channels to gray using the given RGB2GRAY function
 	// gray_val = RGB2Gray(image -> data[pixel+2],image -> data[pixel + 1],	image -> data[pixel]);
@@ -42,7 +45,13 @@ BMPImage * ImgToGray(BMPImage * image){
 	//gray_image->data[pixel+1] = gray_val;
 	//gray_image->data[pixel] = gray_val;
 	//pixel+=3 to move on to the next 3 channel combination
-		
+	while(pixel < (gray_image->header).imagesize){
+		gray_val = RGB2Gray(image -> data[pixel+2],image -> data[pixel + 1],image -> data[pixel]);
+		gray_image->data[pixel+2] = gray_val;
+		gray_image->data[pixel+1] = gray_val;
+		gray_image->data[pixel] = gray_val;
+		pixel+=3;
+	}
 	//return the result
 
 }
