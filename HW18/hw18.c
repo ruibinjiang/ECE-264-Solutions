7865c06c619eba03e4824af78151ec6f25b8b05e
ruibinjiang <jiang551@purdue.edu>
2018-11-27 18:57:09 -0500

    Update HW18

--- HW18/hw18.c
+++ HW18/hw18.c
@@ -64,10 +64,55 @@ void FindMin(ListNode* head)
 
 	/*
 	ENSURE the 2nd parameter of the print function (min1) is smaller than
-	the 3rd parameter (min2). 
+	the 3rd parameter (min2).
 	Look at README, and expected output for more details.
 	*/
+	ListNode *search1 = head;
+	ListNode *search2 = search1->next;
+	ListNode *min1;
+	ListNode *min2;
 
+	int searchDist =  FindDist(search1->treenode, search2->treenode);
+	int minDist = searchDist;
+
+	while(search1 != NULL)
+	{
+		while(search2 != NULL)
+		{
+			searchDist = FindDist(search1->treenode, search2->treenode);
+
+			if (searchDist <= minDist)
+			{
+				minDist = searchDist;
+				min1 = search1;
+				min2 = search2;
+			}
+
+			search2 = search2->next;
+		}
+		search1 = search1->next; //Continue to Analyze Next Element.
+
+		if (search1 != NULL) //Compare to following element.
+		{
+			search2 = search1->next;
+		}
+	}
+
+	int i = 0;
+	while(i < min1->treenode->dimension)
+	{
+		if (min1->treenode->data[i] < min2->treenode->data[i])
+		{
+			PrintAnswer(head,min1,min2);
+			i = min1->treenode->dimension;
+		}
+		else if (min2->treenode->data[i] < min1->treenode->data[i])
+		{
+			PrintAnswer(head,min2,min1);
+			i = min1->treenode->dimension;
+		}
+		++i;
+	}
 }
 #endif
 
@@ -78,6 +123,14 @@ int FindDist(TreeNode* x, TreeNode* y)
 	//find the eucledian distance between
 	// x->data and y->data
 	// DO NOT FIND SQUARE ROOT (we are working with int)
+	int sum = 0;  // must initialize to zero
+	int diff;
+
+	for (int i = 0; i < x -> dimension; i++){
+		diff = x -> data[i] - y -> data[i];
+		sum = sum + diff * diff;
+	}
+	return sum;
 	// return the distance
 }
 #endif
@@ -87,11 +140,30 @@ int FindDist(TreeNode* x, TreeNode* y)
 ListNode* CreateNode(int n, int dim, int* arr)
 {
 	// check for malloc error
+	ListNode*newNode = malloc(sizeof(ListNode));
+	if(newNode==NULL){
+		return NULL;
+	}
 
+	TreeNode*newTNode = malloc(sizeof(TreeNode));
+	if(newTNode==NULL){
+		return NULL;
+	}
 	// initialize dim
+	newTNode->dimension=dim;
 	// both left and right childern will be NULL
+	newTNode->left=NULL;
+	newTNode->right=NULL;
 	// allocate memory for data
+	newTNode->data=malloc(sizeof(int) * dim);
+	for (int i = 0; i < dim; ++i)
+  {
+    newTNode->data[i] = arr[i];
+  }
 	// return a ListNode
+	newNode->next = NULL;
+	newNode->treenode=newTNode;
+	return newNode;
 }
 #endif
 
@@ -100,9 +172,38 @@ ListNode* CreateNode(int n, int dim, int* arr)
 void LinkedListCreate(ListNode ** head, int n, int dim, FILE* fptr)
 {
 	// create temp node using CreateNode
-	// read from file into an array, pass array to CreateNode
-	// assign temp to that node
+	int*arr=malloc(sizeof(int)*dim);
+	if (arr == NULL)
+  {
+    return;
+  }
 
+	for(int i = 0; i<dim;i++){
+		fscanf(fptr, "%d", &arr[i]);
+	}
+
+
+	// read from file into an array, pass array to CreateNode
+	ListNode*tempNode = CreateNode(0,dim,arr);
+	// assign temp to that node
+	*head = tempNode;
 	// use a loop to create nodes for the remaining elements of the list.
+	for (int count = 1; count < n; ++count)
+  {
+    for (int i = 0; i < dim; ++i)
+    {
+      fscanf(fptr, "%d", &arr[i]);
+    }
+
+    while((tempNode->next) != NULL)
+    {
+      tempNode = tempNode->next;
+    }
+
+    tempNode->next = CreateNode(n, dim, arr);
+  }
+
+  free(arr);
+
 }
 #endif
--- HW18/main.c
+++ HW18/main.c
@@ -10,20 +10,30 @@
 int main(int argc, char * * argv)
 {
 	// check for input error
-
+  if (argc < 2)
+  {
+    return EXIT_FAILURE;
+  }
 	// open the file
-
+  FILE *fptr = fopen(argv[1], "r");
 	// input file is in argv[1]
-
+  if (fptr == NULL)
+  {
+    return EXIT_FAILURE;
+  }
 	// n is the element in the file
-
+  int n;
+  fscanf(fptr, "%d", &n);
 	// dim is the second element in the file
-
+  int dim;
+  fscanf(fptr, "%d", &dim);
 	// the rest of the data in the file are the datapoints.
+  ListNode* head = NULL;
 
 	// call LinkedListCreate
-
+  LinkedListCreate(&head,n,dim,fptr);
 	// call FindMin
-
+  FindMin(head);
+  fclose(fptr);
 }
 #endif
